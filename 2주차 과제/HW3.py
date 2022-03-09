0eedcb0a1261629359f711ceb3d449f0166acdf2
yoojud <100751640+yoojud@users.noreply.github.com>
2022-03-09 22:13:56 +0900

    Create HW3.py

---
+++ 2주차 과제/HW3.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+A = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+
+v = np.array([[1],
+              [2],
+              [3]])
+
+w = np.array([[1, 2, 3]])
+
+B = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+print("A.shape =", A.shape)
+print("v.shape =", v.shape)
+print("w.shape =", w.shape)
+print("B.shape =", B.shape)
